6e094cdcdb334f7a99510db3c985ee1329d787cc
lollipop.et <truonglehiep@gmail.com>
2025-11-11 19:22:54 +0700

    feat: Implement player movement and jump controls

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,12 +1,49 @@
 using UnityEngine;
+using UnityEngine.XR;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Player : MonoBehaviour
 {
-    public Rigidbody2D rb;
+    public string playerName = "Hero";
+    private Rigidbody2D rb;
+
+    [SerializeField] private float moveSpeed = 3.5f;
+    [SerializeField] private float jumpForce = 7.0f;
+    private float xInput;
+    private float yInput;
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Start()
+    {
+        Debug.Log("Player " + playerName + " has entered the game.");
+    }
 
     // Update is called once per frame
     void Update()
     {
-        rb.linearVelocity = new Vector2(Input.GetAxis("Horizontal"), rb.linearVelocityY);
+        HandleInput();
+        HandleMovement();
+    }
+
+    private void HandleInput()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        yInput = Input.GetAxis("Vertical");
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void HandleMovement()
+    {
+        rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocityY);
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
     }
 }
